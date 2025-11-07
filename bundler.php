ca68a9e006f8c753a523f591b64b2a31dd515e05
palva39 <paa39@njit.edu>
2025-11-07 20:11:53 +0000

    using PharData to created compressed tar files, and storing them in the repo

--- bundler.php
+++ bundler.php
@@ -6,6 +6,7 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
+$tarFiles_stored = '/home/vboxuser/git/it490-Final-Submission/tarFiles';
 
 $config = parse_ini_file('bundler.ini', true);
 if (!$config) {
@@ -13,18 +14,16 @@ if (!$config) {
     exit(1);
 }
 
-$dir = rtrim($config['deploy']['artifact_folder'], '/');
 $rabbitINI = $config['deploy']['rabbit_ini'];
 $queueName = $config['deploy']['deploy_queue'];
-
 $bundleName = $config['bundle']['name'];
-$files  = $config['bundle']['file.1'];
+$file  = $config['bundle']['file'];
 $description = $config['bundle']['description'] ?? 'No description';
 
 
-if (!file_exists($files)) {
+if (!file_exists($file)) {
     //need to add a checker for all files we are bundling for now one file for testing
-    echo "Error: File $files does not exist.\n";
+    echo "Error: File $file does not exist.\n";
     exit(1);
 }
 
@@ -39,5 +38,19 @@ $response = $client->send_request([
 $nextVersion = $response['version'] ?? 1;
 echo "Next version for $bundleName is $nextVersion\n";
 
+$tarFile = "{$tarFiles_stored }/{$bundleName}_v{$nextVersion}.tar";
+$gzFile  = "{$tarFiles_stored }/{$bundleName}_v{$nextVersion}.tar.gz";
 
+
+try {
+    $phar = new PharData($tarFile);
+    $phar->addFile($file, basename($file));
+    $phar->compress(Phar::GZ);
+    unset($phar);
+    unlink($tarFile);
+    echo "Created bundle: $gzFile\n";
+} catch (Throwable $e) {
+    echo "Error creating tar.gz: {$e->getMessage()}\n";
+    exit(1);
+}
 ?>
