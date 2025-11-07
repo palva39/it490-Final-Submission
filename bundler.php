b4ba730c7ec8469c2903b4365217ba51602f0041
palva39 <paa39@njit.edu>
2025-11-07 19:35:33 +0000

    created varibles that read the ini values in our config file

--- bundler.php
+++ bundler.php
@@ -13,5 +13,13 @@ if (!$config) {
     exit(1);
 }
 
+$dir = rtrim($config['deploy']['artifact_folder'], '/');
+$rabbitINI = $config['deploy']['rabbit_ini'];
+$queueName = $config['deploy']['deploy_queue'];
+
+$bundleName = $config['bundle']['name'];
+$files  = $config['bundle']['file.1'];
+$description = $config['bundle']['description'] ?? 'No description';
+
 
 ?>
